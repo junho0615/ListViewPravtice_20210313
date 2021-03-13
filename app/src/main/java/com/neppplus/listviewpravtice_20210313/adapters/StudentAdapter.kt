cbb37811302aea9ecca68ab93efcc02f141ec04f
홍준호 <0615wnsgh@gmail.com>
2021-03-13 16:16:49 +0900

    ArrayAdapter가 동작하기 위해 필요한 재료를 받아 오고 => 넘겨주기 까지.

--- app/src/main/java/com/neppplus/listviewpravtice_20210313/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listviewpravtice_20210313/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.listviewpravtice_20210313.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.neppplus.listviewpravtice_20210313.datas.Student
 
-class StudentAdapter : ArrayAdapter<Student>() {
+class StudentAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
 }
